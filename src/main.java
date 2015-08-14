fe8c8cfdd5133155de4d47dd1da69fb71ec4112e
NeiroNext <mihail.rozshko@gmail.com>
2015-08-14 22:14:31 +0300

    Reverce function

--- src/main.java
+++ src/main.java
@@ -5,8 +5,15 @@ public class main {
 	public static void main(String[] args){
 		String str = "Hello World!";
 
+		String strRev = "";
+
+		for (int i=str.length()-1; i>=0; i--) {
+			strRev += "" + str.charAt(i);
+		}
+
+
 		for (int i=0; i< 3; i++)
-			System.out.println(str);
+			System.out.println(strRev);
 	}
 
 }
